ecb841794e35d575446b6cbbc64356581e3d8f2a
Sahil <ss6611786@gmail.com>
2023-06-02 12:24:05 +0530

    delete some warnings

--- src/components/Booking.js
+++ src/components/Booking.js
@@ -1,6 +1,6 @@
 import React, { useState} from 'react';
 import { Form, Button, Container, Row, Col } from 'react-bootstrap';
-import { json, useNavigate } from 'react-router-dom';
+import {useNavigate } from 'react-router-dom';
 
 function Booking({movie}) {
     const navigate=useNavigate();
